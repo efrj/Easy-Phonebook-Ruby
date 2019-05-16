6260dcd4cf32469c1ff4161839e1c5b2fff731d0
efrj <efrj@hotmail.com>
2019-05-15 23:21:45 -0300

    Adjust in tabs.

--- app.rb
+++ app.rb
@@ -6,41 +6,44 @@ require_relative 'helpers/contacts_helper.rb'
 enable :sessions
 
 get '/' do
-	erb :index
+  erb :index
 end
 
 post '/contact/save' do
-	@contact = Contact.new
-	if params[:letter_contact] and params[:xml_file]
-		@contact.dropFile params[:letter_contact], params[:xml_file]
-	end
+  contact = Contact.new
+  if params[:letter_contact] and params[:xml_file]
+    contact.dropFile params[:letter_contact], params[:xml_file]
+  end
 
-	@new_contact = @contact.save(params[:contact_name], params[:contact_email], params[:contact_birthdate], params[:contact_phone_number])
+  new_contact = contact.save(params[:contact_name],
+    params[:contact_email],
+    params[:contact_birthdate],
+    params[:contact_phone_number])
 
-	flash[:notice] = 'The contact was saved successfully.'
-	redirect '/letter/' << @new_contact
+  flash[:notice] = 'The contact was saved successfully.'
+  redirect '/letter/' << new_contact
 end
 
 get '/letter/:letter' do
-	@contacts = Contact.new
-	@files = @contacts.findAll params[:letter]
-	@letter = params[:letter]
-	erb :list
+  contacts = Contact.new
+  @files = contacts.findAll params[:letter]
+  @letter = params[:letter]
+  erb :list
 end
 
 get '/contact/show/:letter/:file' do
-	@letter_contact = params[:letter]
-	@xml_file = params[:file]
-	@contact = Contact.new
-	@file = @contact.find @letter_contact, @xml_file
-	erb :show, :layout => false do
-		erb :show
-	end
+  @letter_contact = params[:letter]
+  xml_file = params[:file]
+  contact = Contact.new
+  @file = contact.find @letter_contact, xml_file
+  erb :show, :layout => false do
+    erb :show
+  end
 end
 
 get '/contact/delete/:letter/:file' do
-	@contact = Contact.new
-	@contact.dropFile params[:letter], params[:file]
-	flash[:notice] = 'The contact was deleted successfully.'
-	redirect '/letter/' << params[:letter]
+  contact = Contact.new
+  contact.dropFile params[:letter], params[:file]
+  flash[:notice] = 'The contact was deleted successfully.'
+  redirect '/letter/' << params[:letter]
 end
--- models/contact.rb
+++ models/contact.rb
@@ -1,59 +1,59 @@
 class Contact
-	def name
-		@name
-	end
+  def name
+    @name
+  end
 
-	def email
-		@email
-	end
+  def email
+    @email
+  end
 
-	def birthdate
-		@birthdate
-	end
+  def birthdate
+    @birthdate
+  end
 
-	def phone_number
-		@phone_number
-	end
+  def phone_number
+    @phone_number
+  end
 
-	def findAll letter
-		@files = Dir.glob('xml/letters/' << letter <<'/*.xml')
-		return @files
-	end
+  def findAll letter
+    @files = Dir.glob('xml/letters/' << letter <<'/*.xml')
+    return @files
+  end
 
-	def save name, email, birthdate, phone_number
-		require 'nokogiri'
-		@name = name
-		@email = email
-		@birthdate = birthdate
-		@phone_number = phone_number
+  def save name, email, birthdate, phone_number
+    require 'nokogiri'
+    @name = name
+    @email = email
+    @birthdate = birthdate
+    @phone_number = phone_number
 
-		builder = Nokogiri::XML::Builder.new do |xml|
-			xml.contact {
-				xml.name @name
-				xml.email @email
-				xml.birthdate @birthdate
-				xml.phone_number @phone_number
-			}
-		end
-		xml_string = builder.to_xml
-		letter_contact = name[0,1].downcase!
-		Dir.mkdir('xml/letters/' << letter_contact) unless File.exists?('xml/letters/' << letter_contact)
-		file_name = name.gsub(' ', '_').downcase!
-		File.open('xml/letters/' << letter_contact << '/' << file_name << '.xml', 'w+') do |file|
-			file.write xml_string
-		end
-		return letter_contact
-	end
+    builder = Nokogiri::XML::Builder.new do |xml|
+      xml.contact {
+        xml.name @name
+        xml.email @email
+        xml.birthdate @birthdate
+        xml.phone_number @phone_number
+      }
+    end
+    xml_string = builder.to_xml
+    letter_contact = name[0,1].downcase!
+    Dir.mkdir('xml/letters/' << letter_contact) unless File.exists?('xml/letters/' << letter_contact)
+    file_name = name.gsub(' ', '_').downcase!
+    File.open('xml/letters/' << letter_contact << '/' << file_name << '.xml', 'w+') do |file|
+      file.write xml_string
+    end
+    return letter_contact
+  end
 
-	def find letter, file
-		require 'nokogiri'
-		@file = Nokogiri::XML(File.open('xml/letters/' << letter << '/' << file << '.xml'))
-		return @file
-	end
+  def find letter, file
+    require 'nokogiri'
+    @file = Nokogiri::XML(File.open('xml/letters/' << letter << '/' << file << '.xml'))
+    return @file
+  end
 
-	def dropFile letter, file
-		if File.exist?('xml/letters/' << letter << '/' << file << '.xml')
-			File.delete('xml/letters/' << letter << '/' << file << '.xml')
-		end
-	end
+  def dropFile letter, file
+    if File.exist?('xml/letters/' << letter << '/' << file << '.xml')
+      File.delete('xml/letters/' << letter << '/' << file << '.xml')
+    end
+  end
 end
