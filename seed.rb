c2884b69336e20022c80512a817ea3a65c3e0f42
Edivaldo Reis <efrj@hotmail.com>
2019-05-30 10:47:45 -0300

    Update seed.rb
    Adjust Settings in the tab.

--- seed.rb
+++ seed.rb
@@ -7,16 +7,16 @@ i = 0
 total = 50
 
 until i > total  do
-	i +=1;
-	name = names.shuffle[0]
-	surname = surnames.shuffle[0] << ' ' << surnames.shuffle[1]
-	fullName = name << ' ' << surname
-	email = fullName.downcase.gsub(' ', '_').gsub('\'', '') << '@example.com'
-	birthdate = (Time.now - rand(15552000)).strftime('%Y/%m/%d')
-	numberRandom = Random.new
-	phone = '9' << numberRandom.rand(0..9).to_s << numberRandom.rand(0..9).to_s << numberRandom.rand(0..9).to_s << '-' << numberRandom.rand(0..9).to_s << numberRandom.rand(0..9).to_s << numberRandom.rand(0..9).to_s << numberRandom.rand(0..9).to_s
+  i +=1;
+  name = names.shuffle[0]
+  surname = surnames.shuffle[0] << ' ' << surnames.shuffle[1]
+  fullName = name << ' ' << surname
+  email = fullName.downcase.gsub(' ', '_').gsub('\'', '') << '@example.com'
+  birthdate = (Time.now - rand(15552000)).strftime('%Y/%m/%d')
+  numberRandom = Random.new
+  phone = '9' << numberRandom.rand(0..9).to_s << numberRandom.rand(0..9).to_s << numberRandom.rand(0..9).to_s << '-' << numberRandom.rand(0..9).to_s << numberRandom.rand(0..9).to_s << numberRandom.rand(0..9).to_s << numberRandom.rand(0..9).to_s
 
-	@contact = Contact.new
-	@new_contact = @contact.save(fullName, email, birthdate, phone)
-	puts 'Contact create: ' << fullName
+  @contact = Contact.new
+  @new_contact = @contact.save(fullName, email, birthdate, phone)
+  puts 'Contact create: ' << fullName
 end
