50a5e9fd313b87ed8cb5d75df0129edfeebbd6ce
efrj <efrj@hotmail.com>
2019-05-31 10:38:46 -0300

    Adjust in variable nomenclatures

--- seed.rb
+++ seed.rb
@@ -9,14 +9,14 @@ total = 50
 until i > total  do
   i +=1;
   name = names.shuffle[0]
-  surname = surnames.shuffle[0] << ' ' << surnames.shuffle[1]
-  fullName = name << ' ' << surname
-  email = fullName.downcase.gsub(' ', '_').gsub('\'', '') << '@example.com'
+  surname = surnames.shuffle[0] + ' ' + surnames.shuffle[1]
+  full_name = name + ' ' + surname
+  email = full_name.downcase.gsub(' ', '_').gsub('\'', '') + '@example.com'
   birthdate = (Time.now - rand(15552000)).strftime('%Y/%m/%d')
-  numberRandom = Random.new
-  phone = '9' << numberRandom.rand(0..9).to_s << numberRandom.rand(0..9).to_s << numberRandom.rand(0..9).to_s << '-' << numberRandom.rand(0..9).to_s << numberRandom.rand(0..9).to_s << numberRandom.rand(0..9).to_s << numberRandom.rand(0..9).to_s
+  phone_number_random = Random.new
+  phone = '9' << phone_number_random.rand(0..9).to_s << phone_number_random.rand(0..9).to_s << phone_number_random.rand(0..9).to_s << '-' << phone_number_random.rand(0..9).to_s << phone_number_random.rand(0..9).to_s << phone_number_random.rand(0..9).to_s << phone_number_random.rand(0..9).to_s
 
-  @contact = Contact.new
-  @new_contact = @contact.save(fullName, email, birthdate, phone)
-  puts 'Contact create: ' << fullName
+  contact = Contact.new
+  new_contact = contact.save(full_name, email, birthdate, phone)
+  puts 'Contact create: ' + full_name
 end
